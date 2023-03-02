db8a4b6c9dd3912fe15a2dc39ca2b2e7d8b48506
maxi0272 <maximilian.b.shf@gmail.com>
2023-03-02 13:08:34 +0100

    Made enemy speed variable work. No Move_Toward

--- Game/World/Enemies/Enemy 1.gd
+++ Game/World/Enemies/Enemy 1.gd
@@ -3,21 +3,22 @@ extends KinematicBody2D
 var distoplay
 
 var move = Vector2.ZERO
-
 var animation = Sprite
-var velocity = Vector2.ZERO
 var speed = 100
 var pos = Vector2.ZERO
 
 
 
 
+
 # Called when the node enters the scene tree for the first time.
-func _physics_process(_delta):
+func _physics_process(delta):
 	var Player = get_parent().get_node("Player")
 	pos = Player.position - position
 	pos = pos.normalized()
+	pos = pos * delta * speed
 	position += pos
+	
 
 
 func _on_Hurtbox_area_entered(_area):
