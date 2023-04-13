dabec7c9d28e75fb4c74b0169fdae7a78a184337
maxi0272 <maximilian.b.shf@gmail.com>
2023-04-13 10:09:23 +0200

    Made bullets work again added following camera

--- Game/World/Enemies/Bullet_enemy.gd
+++ Game/World/Enemies/Bullet_enemy.gd
@@ -1,10 +1,11 @@
 extends RigidBody2D
 
-
+#var scene =load("res://World/Player.tscn")
 var pos = Vector2.ZERO
-onready var dir = (get_node("res://World/Enemies/Bullet_enemy.tscn").global_position - global_position).normalized()
+onready var dir = (get_parent().get_parent().get_node("Player").global_position - global_position).normalized()
 var speed : float = 100
 
 
 func _process(delta):
 	translate(dir*speed*delta)
+
